732f674c4d1ec1e983ab990ab52978956e049001
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-10-16 13:56:34 +0200

    add netlify api key

--- chatgpt.js
+++ chatgpt.js
@@ -1,20 +1,5 @@
-// function highlightDifferences(text2, text1) {
-//     const words1 = text1.trim().split(/\s+/);
-//     const words2 = text2.trim().split(/\s+/);
 
-//     let result = '';
-//     const maxLength = Math.max(words1.length, words2.length);
-
-//     for (let i = 0; i < maxLength; i++) {
-//       if (words1[i] !== words2[i]) {
-//         result += `<span style="background-color: yellow" >${words2[i] || ''}</span> `;
-//       } else {
-//         result += words2[i] ? `${words2[i]} ` : ' ';
-//       }
-//     }
-
-//     return result;
-// }
+const OPENAI_API_KEY = Netlify.env.get("OPENAI_API_KEY");
 
 function Send1(command) {
   // var sQuestion = txtMsg.value;
